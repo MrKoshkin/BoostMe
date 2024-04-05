d722fe867b07256592cc40f4781627e2447c4a91
MrKoshkin <Kosh.job@gmail.com>
2024-04-05 10:56:39 +0600

    Initial commit

---
+++ src/main/java/BoostMe/BoostMeApplication.java
@@ -0,0 +1,13 @@
+package java.BoostMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoostMeApplication.class, args);
+	}
+
+}
