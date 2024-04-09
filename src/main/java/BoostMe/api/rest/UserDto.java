dfb97bb8b52020ed9b15848b1b670ee3df6c80bd
MrKoshkin <Kosh.job@gmail.com>
2024-04-09 11:39:13 +0600

    Added user dto

---
+++ src/main/java/BoostMe/api/rest/UserDto.java
@@ -0,0 +1,12 @@
+package BoostMe.api.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+}
